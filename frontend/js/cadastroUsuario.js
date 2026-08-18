0e2fac9e2cd44b44e11708bdc555db3ef2855054
Carlos <silva@edu.sc.senai.br>
2026-08-18 08:38:48 -0300

    criando o card

--- frontend/js/cadastroUsuario.js
+++ frontend/js/cadastroUsuario.js
@@ -13,5 +13,38 @@ btn_cadastrar.addEventListener('click', (e)=>{
         return
     }
 
+    const valores = {
+        nome: nome,
+        email: email,
+        senha: senha
+    }
+
+    fetch(`${API_URL}/usuario`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(valores)
+    })
+    .then(resp => resp.json())
+    .then(dados =>{
+        console.log(dados)
+
+        resposta.innerHTML = ''
+        resposta.innerHTML += `<p>${dados.message}</p><br>`
+        resposta.innerHTML += `<div class="card">${criarCard(dados)}</div>`
+    })
 })
 
+
+function criarCard(dados){
+    let card = ''
+
+    card += `    
+        <h2>Nome: ${dados.dados.nome}</h2>
+        <hr>
+        <br>
+        <p><strong>Código: ${dados.dados.codUsuario}</strong></p>
+        <p><strong>email: ${dados.dados.email}</strong></p>
+        <p><strong>senha: ${dados.dados.senha}</strong></p>        
+    `
+    return card
+}
